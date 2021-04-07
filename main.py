e6b44b591005631f8cadb6e9359ab4a2c42d7c23
Salvador Guzman <guzmansalv@gmail.com>
2021-04-07 15:54:34 -0700

    working main stub

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from typing import List
 
 
-def triple(alist, blist, clist):
+def quadf(alist, blist, clist, dlist, func):
     for a in alist:
         for b in blist:
             for c in clist:
-                yield a, b, c
-
-
-def double(alist, blist):
-    for a in alist:
-        for b in blist:
-            yield a, b
+                for d in dlist:
+                    yield func(a, b, c, d)
 
 
 def triplef(alist, blist, clist, func):
@@ -27,10 +22,11 @@ def doublef(alist, blist, func):
             yield func(a, b)
 
 
-def eci_url(a, b, c):
+def eci_url(a, b, c,):
     url: str = f'https://oec.world/olap-proxy/data.jsonrecords?' \
                f'cube=complexity_eci_a_{b}_{a}&' \
-               f'Year={c}&' \
+               f'Year=2020&' \
+               f'Country={c}&' \
                f'drilldowns=Country,Year,ECI+Rank&' \
                f'measures=ECI&' \
                f'parents=true&' \
@@ -39,14 +35,45 @@ def eci_url(a, b, c):
     return url
 
 
-def pci_url(a, b, c):
+def pci_url_hs4(a, b):
     url: str = f'https://oec.world/olap-proxy/data.jsonrecords?' \
-               f'cube=complexity_pci_a_{b}_{a}&' \
-               f'Year={c}&' \
+               f'cube=complexity_pci_a_{a}_hs4&' \
+               f'Year=2020&' \
+               f'HS4={b}&' \
+               f'drilldowns=HS4,PCI+Rank,Year&' \
+               f'measures=PCI&' \
+               f'parents=true&' \
+               f'sparse=true'
+
+    return url
+
+
+def pci_url_hs6(a, b):
+    url: str = f'https://oec.world/olap-proxy/data.jsonrecords?' \
+               f'cube=complexity_pci_a_{a}_hs6&' \
+               f'Year=2020&' \
+               f'HS6={b}&' \
                f'drilldowns=HS6,PCI+Rank,Year&' \
                f'measures=PCI&' \
                f'parents=true&' \
                f'sparse=true'
+
+    return url
+
+
+def tarrifs_url(a, b, c, d):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'HS6={a}&' \
+               f'Reporter+Country={b}&' \
+               f'Year={c}&' \
+               f'Partner+Country={d}&' \
+               f'cube=tariffs_i_wits_a_hs_new&' \
+               f'drilldowns=Year,HS6,Partner+Country,Reporter+Country,Agreement&' \
+               f'measures=Tariff&' \
+               f'parents=true&' \
+               f'sparse=true&' \
+               f'sort=Tariff.desc&' \
+               f'locale=en'
 
     return url
 
@@ -62,14 +89,26 @@ def main() -> None:
         'hs07',
     ]
 
-    years = range(1965, 2021)
-    hs6: List[str] = open('./hs6.csv').readlines()
-    countries: List[str] = open('./country-code.csv').readlines()
+    depth2: List[str] = ['hs2', 'hs4', 'hs6']
+    rev2: List[str] = [
+        'hs92',
+        'hs96'
+    ]
 
-    for a in triplef(depth, rev, years, eci_url):
+    hs4: List[str] = [s.removesuffix('\n') for s in open('./hs4.csv').readlines()]
+    hs6: List[str] = [s.removesuffix('\n') for s in open('./hs6.csv').readlines()]
+    countries: List[str] = [s.removesuffix('\n') for s in open('./country-code.csv').readlines()]
+
+    # 1
+    for a in triplef(depth, rev, countries, eci_url):
         print(a)
 
-    for a in triplef(depth, rev, years, pci_url):
+    # 2
+    for a in doublef(rev, hs4, pci_url_hs4):
+        print(a)
+
+    # 3
+    for a in doublef(rev, hs6, pci_url_hs6):
         print(a)
 
 
