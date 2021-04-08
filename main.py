a8957b81a118684e29eafe62bb24a5f1adec7220
Salvador Guzman <guzmansalv@gmail.com>
2021-04-08 00:54:26 -0700

    added services and other junk

--- main.py
+++ main.py
@@ -23,11 +23,14 @@ def doublef(alist, blist, func):
             yield func(a, b)
 
 
-def eci_url(a, b, c,):
+def singlef(alist, func):
+    for a in alist:
+        yield func(a)
+
+
+def eci_url(a, b):
     url: str = f'https://oec.world/olap-proxy/data.jsonrecords?' \
                f'cube=complexity_eci_a_{b}_{a}&' \
-               f'Year=2020&' \
-               f'Country={c}&' \
                f'drilldowns=Country,Year,ECI+Rank&' \
                f'measures=ECI&' \
                f'parents=true&' \
@@ -39,7 +42,6 @@ def eci_url(a, b, c,):
 def pci_url_hs4(a, b):
     url: str = f'https://oec.world/olap-proxy/data.jsonrecords?' \
                f'cube=complexity_pci_a_{a}_hs4&' \
-               f'Year=2020&' \
                f'HS4={b}&' \
                f'drilldowns=HS4,PCI+Rank,Year&' \
                f'measures=PCI&' \
@@ -52,7 +54,6 @@ def pci_url_hs4(a, b):
 def pci_url_hs6(a, b):
     url: str = f'https://oec.world/olap-proxy/data.jsonrecords?' \
                f'cube=complexity_pci_a_{a}_hs6&' \
-               f'Year=2020&' \
                f'HS6={b}&' \
                f'drilldowns=HS6,PCI+Rank,Year&' \
                f'measures=PCI&' \
@@ -62,12 +63,9 @@ def pci_url_hs6(a, b):
     return url
 
 
-def tarrifs_url_hs2(a, b):
-
+def tarrifs_url_hs2(a):
     url: str = f'https://oec.world/olap-proxy/data?' \
                f'HS2={a}&' \
-               f'Reporter+Country={b}&' \
-               f'Year=2020&' \
                f'cube=tariffs_i_wits_a_hs_new&' \
                f'drilldowns=Year,HS2,Partner+Country,Reporter+Country,Agreement&' \
                f'measures=Tariff&' \
@@ -79,11 +77,9 @@ def tarrifs_url_hs2(a, b):
     return url
 
 
-def tarrifs_url_hs4(a, b):
+def tarrifs_url_hs4(a):
     url: str = f'https://oec.world/olap-proxy/data?' \
                f'HS4={a}&' \
-               f'Reporter+Country={b}&' \
-               f'Year=2020&' \
                f'cube=tariffs_i_wits_a_hs_new&' \
                f'drilldowns=Year,HS4,Partner+Country,Reporter+Country,Agreement&' \
                f'measures=Tariff&' \
@@ -95,11 +91,9 @@ def tarrifs_url_hs4(a, b):
     return url
 
 
-def tarrifs_url_hs6(a, b):
+def tarrifs_url_hs6(a):
     url: str = f'https://oec.world/olap-proxy/data?' \
                f'HS6={a}&' \
-               f'Reporter+Country={b}&' \
-               f'Year=2020&' \
                f'cube=tariffs_i_wits_a_hs_new&' \
                f'drilldowns=Year,HS4,Partner+Country,Reporter+Country,Agreement&' \
                f'measures=Tariff&' \
@@ -107,6 +101,128 @@ def tarrifs_url_hs6(a, b):
                f'sparse=true&' \
                f'sort=Tariff.desc&' \
                f'locale=en'
+
+    return url
+
+
+def import_hs2(a, b):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_baci_a_{a}&' \
+               f'drilldowns=Year,HS2,Exporter+Country,Importer+Country&' \
+               f'measures=Quantity,Trade+Value&' \
+               f'parents=true&' \
+               f'sparse=true&' \
+               f'locale=en&' \
+               f'HS2={b}'
+
+    return url
+
+
+def import_hs4(a, b):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_baci_a_{a}&' \
+               f'drilldowns=Year,HS4,Exporter+Country,Importer+Country&' \
+               f'measures=Quantity,Trade+Value&' \
+               f'parents=true&' \
+               f'sparse=true&' \
+               f'locale=en&' \
+               f'HS4={b}'
+
+    return url
+
+
+def import_hs6(a, b):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_baci_a_{a}&' \
+               f'drilldowns=Year,HS6,Exporter+Country,Importer+Country&' \
+               f'measures=Quantity,Trade+Value&' \
+               f'parents=true&' \
+               f'sparse=true&' \
+               f'locale=en&' \
+               f'HS6={b}'
+
+    return url
+
+
+def monthly_1_hs2(a):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_comtrade_m_hs&' \
+               f'Trade+Flow=1&' \
+               f'drilldowns=Time,Trade+Flow,Reporter+Country,Partner+Country,HS2&' \
+               f'measures=Trade+Value,Net+Weight&' \
+               f'locale=en&' \
+               f'HS2={a}'
+
+    return url
+
+
+def monthly_1_hs4(a):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_comtrade_m_hs&' \
+               f'Trade+Flow=1&' \
+               f'drilldowns=Time,Trade+Flow,Reporter+Country,Partner+Country,HS4&' \
+               f'measures=Trade+Value,Net+Weight&' \
+               f'locale=en&' \
+               f'HS4={a}'
+
+    return url
+
+def monthly_1_hs6(a):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_comtrade_m_hs&' \
+               f'Trade+Flow=1&' \
+               f'drilldowns=Time,Trade+Flow,Reporter+Country,Partner+Country,HS6&' \
+               f'measures=Trade+Value,Net+Weight&' \
+               f'locale=en&' \
+               f'HS6={a}'
+
+    return url
+
+
+def monthly_2_hs2(a):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_comtrade_m_hs&' \
+               f'Trade+Flow=2&' \
+               f'drilldowns=Time,Trade+Flow,Reporter+Country,Partner+Country,HS2&' \
+               f'measures=Trade+Value,Net+Weight&' \
+               f'locale=en&' \
+               f'HS2={a}'
+
+    return url
+
+
+def monthly_2_hs4(a):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_comtrade_m_hs&' \
+               f'Trade+Flow=2&' \
+               f'drilldowns=Time,Trade+Flow,Reporter+Country,Partner+Country,HS4&' \
+               f'measures=Trade+Value,Net+Weight&' \
+               f'locale=en&' \
+               f'HS4={a}'
+
+    return url
+
+
+def monthly_2_hs6(a):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=trade_i_comtrade_m_hs&' \
+               f'Trade+Flow=2&' \
+               f'drilldowns=Time,Trade+Flow,Reporter+Country,Partner+Country,HS6&' \
+               f'measures=Trade+Value,Net+Weight&' \
+               f'locale=en&' \
+               f'HS6={a}'
+
+    return url
+
+
+def services(a):
+    url: str = f'https://oec.world/olap-proxy/data?' \
+               f'cube=services_i_comtrade_a_eb02&' \
+               f'Reporter+Country={a}&' \
+               f'drilldowns=Year,Trade+Flow,Reporter+Country,Service,Partner+Country&' \
+               f'measures=Service+Value&' \
+               f'parents=true&' \
+               f'sparse=true'
 
     return url
 
@@ -134,7 +250,7 @@ def main() -> None:
     countries: List[str] = [s.removesuffix('\n') for s in open('./country-code.csv').readlines()]
 
     # 1
-    for a in triplef(depth, rev, countries, eci_url):
+    for a in doublef(depth, rev, eci_url):
         print(a)
 
     # ---
@@ -149,13 +265,52 @@ def main() -> None:
 
     # ---
 
-    for a in doublef(hs2, countries, tarrifs_url_hs2):
+    for a in singlef(hs2, tarrifs_url_hs2):
         print(a)
 
-    for a in doublef(hs4, countries, tarrifs_url_hs4):
+    for a in singlef(hs4, tarrifs_url_hs4):
         print(a)
 
-    for a in doublef(hs6, countries, tarrifs_url_hs6):
+    for a in singlef(hs6, tarrifs_url_hs6):
+        print(a)
+
+
+    # ---
+
+    for a in doublef(rev, hs2, import_hs2):
+        print(a)
+
+    for a in doublef(rev, hs4, import_hs4):
+        print(a)
+
+    for a in doublef(rev, hs6, import_hs6):
+        print(a)
+
+    # ---
+
+    for a in singlef(hs2, monthly_1_hs2):
+        print(a)
+
+    for a in singlef(hs4, monthly_1_hs4):
+        print(a)
+
+    for a in singlef(hs6, monthly_1_hs6):
+        print(a)
+
+    # ---
+
+    for a in singlef(hs2, monthly_2_hs2):
+        print(a)
+
+    for a in singlef(hs4, monthly_2_hs4):
+        print(a)
+
+    for a in singlef(hs6, monthly_2_hs6):
+        print(a)
+
+    # ---
+
+    for a in singlef(countries, services):
         print(a)
 
 
